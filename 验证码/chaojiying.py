0a01073829c6c1149fc1cfe95729c80edd2e9de6
DeemoASCII <xxb1021054331@gmail.com>
2018-05-13 10:29:53 +0800

    xiaoxiangbin

--- 验证码/chaojiying.py
+++ 验证码/chaojiying.py
@@ -1,15 +1,12 @@
-#!/usr/bin/env python
-# coding:utf-8
-
 import requests
 from hashlib import md5
 
 
-class Chaojiying_Client(object):
+class Chaojiying(object):
 
     def __init__(self, username, password, soft_id):
         self.username = username
-        self.password = md5(password).hexdigest()
+        self.password = md5(password.encode('utf-8')).hexdigest()
         self.soft_id = soft_id
         self.base_params = {
             'user': self.username,
@@ -21,7 +18,8 @@ class Chaojiying_Client(object):
             'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0)',
         }
 
-    def PostPic(self, im, codetype):
+
+    def post_pic(self, im, codetype):
         """
         im: 图片字节
         codetype: 题目类型 参考 http://www.chaojiying.com/price.html
@@ -34,7 +32,7 @@ class Chaojiying_Client(object):
         r = requests.post('http://upload.chaojiying.net/Upload/Processing.php', data=params, files=files, headers=self.headers)
         return r.json()
 
-    def ReportError(self, im_id):
+    def report_error(self, im_id):
         """
         im_id:报错题目的图片ID
         """
@@ -44,10 +42,4 @@ class Chaojiying_Client(object):
         params.update(self.base_params)
         r = requests.post('http://upload.chaojiying.net/Upload/ReportError.php', data=params, headers=self.headers)
         return r.json()
-
-
-if __name__ == '__main__':
-    chaojiying = Chaojiying_Client('超级鹰用户名', '超级鹰用户名的密码', '96001')
-    im = open('a.jpg', 'rb').read()
-    print chaojiying.PostPic(im, 1902)
 
--- 验证码/点触验证码.py
+++ 验证码/点触验证码.py
@@ -14,3 +14,94 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from chaojiying import Chaojiying
+
+Chaojiying_username = '1021054331'
+Chaojiying_password = '400810520'
+Chaojiying_soft_id = 896503
+Chaojiying_kind = 9004
+
+class CrackTouClick():
+    def __init__(self):
+        self.url = 'http://dun.163.com/trial/picture-click'
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver,10,0.5)
+        self.chaojiying = Chaojiying(Chaojiying_username,Chaojiying_password,Chaojiying_soft_id)
+
+    def __del__(self):
+        self.driver.close()
+
+    def open(self):
+        '''
+        打开网页 最大化 操作滚动条
+        :return:
+        '''
+        self.driver.get(self.url)
+        self.driver.maximize_window()
+        time.sleep(3)
+        js = 'var q=document.documentElement.scrollTop=100'
+        self.driver.execute_script(js)
+
+    def get_touclick_element(self):
+        element = self.wait.until(EC.presence_of_element_located((By.XPATH,'/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div[3]/div/div/div[1]/div/div[1]/img[1]')))
+        return element
+
+    def get_points(self,result):
+        groups = result.get('pic_str').split('|')
+        locations = [[int(number) for number in group.split(',')]for group in groups]
+        # print(locations)
+        return locations
+
+    def touch_click_words(self,locations):
+        if len(locations) == 3:
+            print(locations)
+            for location in locations:
+                ActionChains(self.driver).move_to_element_with_offset(self.get_touclick_element(),location[0],location[1]).click().perform()
+                time.sleep(1)
+        else:
+            s = self.driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div[3]/div/div/div[1]/div/div[3]')
+            s.click()
+            self.main()
+
+    def get_touclick_image(self,name='captcha.png'):
+        '''
+        获取图片
+        :param name:
+        :return:
+        '''
+        time.sleep(2)
+        self.driver.save_screenshot('aa.png')
+        element = self.driver.find_element_by_xpath('/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div[3]/div/div')
+        left = element.location['x']
+        top = element.location['y'] - 100
+        right = element.location['x'] + element.size['width']
+        bottom = element.location['y'] + element.size['height'] - 100
+        im = Image.open('aa.png')
+        captcha = im.crop((left,top,right,bottom))
+        # captcha.show()
+        captcha.save(name)
+        return captcha
+
+    def main(self):
+        self.get_touclick_element()
+        image = self.get_touclick_image()
+        bytes_arry = BytesIO()
+        image.save(bytes_arry,format('PNG'))
+        result = self.chaojiying.post_pic(bytes_arry.getvalue(),Chaojiying_kind)
+        # print(result)
+        locations = self.get_points(result)
+        self.touch_click_words(locations)
+        # time.sleep(10)
+        try:
+            success = self.wait.until(EC.text_to_be_present_in_element((By.XPATH,'/html/body/div[2]/div/div[2]/div[2]/div[3]/div/div[2]/div[3]/div/div/div[2]/div[3]/span[2]'),'验证成功'))
+        except:
+            print('验证失败，重新上传验证')
+            self.main()
+        else:
+            self.driver.save_screenshot('True.png')
+            print(success)
+
+if __name__ == '__main__':
+    crack = CrackTouClick()
+    crack.open()
+    crack.main()
